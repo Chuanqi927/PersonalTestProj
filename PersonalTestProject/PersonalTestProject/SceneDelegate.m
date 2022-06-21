5ceb811c9b6752e325d27d0acedc5922f675716e
Chuanqi927 <xcq927xcq@gmail.com>
2022-06-21 16:53:04 +0800

    Enter from window instead of storyboard

--- PersonalTestProject/PersonalTestProject/SceneDelegate.m
+++ PersonalTestProject/PersonalTestProject/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -18,6 +19,15 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    UIWindowScene *windowScene = (UIWindowScene*)scene;
+    if (!windowScene) {
+        windowScene = [[UIWindowScene alloc] initWithSession:session connectionOptions:connectionOptions];
+    }
+    self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    ViewController *vc = [[ViewController alloc] init];
+    UINavigationController *baseNavVC = [[UINavigationController alloc] initWithRootViewController:vc];
+    self.window.rootViewController = baseNavVC;
+    [self.window makeKeyAndVisible];
 }
 
 
